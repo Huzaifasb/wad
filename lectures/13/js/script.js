068130a9d9a4d234ba787854d786090fbae6ce89
Huzaifasb <45456524+Huzaifasb@users.noreply.github.com>
2018-11-29 17:30:59 +0500

    Update script.js

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -19,20 +19,102 @@ var questions = [{
         "for",
         "none of the above"],
     correctAnswer : 1
+    question: "Who is the strongest?",
+    choices: {
+         "Superman",
+         "The Terminator",
+         "Waluigi, obviously"
+    },
+    correctAnswer: "3"
+},
+    {
+        question: "What is the best site ever created?",
+        choices: {
+             "SitePoint",
+             "Simple Steps Code",
+             "Trick question; they're both the best"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "Where is Waldo really?",
+        choices: {
+             "Antarctica",
+             "Exploring the Pacific Ocean",
+             "Sitting in a tree",
+             "Minding his own business, so stop asking"
+        },
+        correctAnswer: "4"
+    }
 }];
 
 var currentQuestion = 0;
 var correctAnswers = 0;
+var questionNumber=1;
 var quizOver = false;
 displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
-    /*Write your code here */
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        // if answer is correct
+        if(userAnswer===currentQuestion.correctAnswer){
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+            // color the answers red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
 }
 
 function displayCurrentQuestion() {
-    /*Write your code here */
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // we'll want to store the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
+
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+            );
+        } \
+
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
 }
+}
+
 
 function resetQuiz() {
     currentQuestion = 0;
